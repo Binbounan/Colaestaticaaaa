7b90d414b6e6e9e0c0e66d65b2d3b052f6f35ffe
Binbounan <binpounan@gmail.com>
2023-10-11 20:44:23 -0600

    busqueda

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ int main()
 {
     cola MiCola;
     alumno x;
-    for(int i = 1; i <= 2; i++){
+    for(int i = 1; i <= 5; i++){
         std::cin >> x;
         MiCola.enqueue(x);
     }
-    for(int i = 1; i <= 2; i++)
+    for(int i = 1; i <= 5; i++)
         std::cout << MiCola.dequeue();
 
     return 0;
